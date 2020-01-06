91e787fc25e8b7733f1bcd6cbdde04c2351d79bc
Craig Gumbley <craiggumbley@gmail.com>
2020-01-06 14:01:32 +0000

    Remove comments

--- KeyVault.Acmebot/SharedFunctions.cs
+++ KeyVault.Acmebot/SharedFunctions.cs
@@ -57,30 +57,23 @@ namespace KeyVault.Acmebot
 
             var activity = context.CreateActivityProxy<ISharedFunctions>();
 
-            // 前提条件をチェック
             await activity.Dns01Precondition(dnsNames);
 
-            // 新しく ACME Order を作成する
             var orderDetails = await activity.Order(dnsNames);
 
-            // 複数の Authorizations を処理する
             var challenges = new List<ChallengeResult>();
 
             foreach (var authorization in orderDetails.Payload.Authorizations)
             {
-                // ACME Challenge を実行
                 var result = await activity.Dns01Authorization((authorization, context.ParentInstanceId ?? context.InstanceId));
 
-                // Azure DNS で正しくレコードが引けるか確認
                 await activity.CheckDnsChallenge(result);
 
                 challenges.Add(result);
             }
 
-            // ACME Answer を実行
             await activity.AnswerChallenges(challenges);
 
-            // Order のステータスが ready になるまで 60 秒待機
             await activity.CheckIsReady(orderDetails);
 
             await activity.FinalizeOrder((dnsNames, orderDetails));
@@ -122,7 +115,6 @@ namespace KeyVault.Acmebot
         [FunctionName(nameof(Dns01Precondition))]
         public async Task Dns01Precondition([ActivityTrigger] string[] hostNames)
         {
-            // Azure DNS が存在するか確認
             var zones = await _dnsManagementClient.Zones.ListAllAsync();
 
             foreach (var hostName in hostNames)
@@ -143,17 +135,14 @@ namespace KeyVault.Acmebot
 
             var authz = await acmeProtocolClient.GetAuthorizationDetailsAsync(authzUrl);
 
-            // DNS-01 Challenge の情報を拾う
             var challenge = authz.Challenges.First(x => x.Type == "dns-01");
 
             var challengeValidationDetails = AuthorizationDecoder.ResolveChallengeForDns01(authz, challenge, acmeProtocolClient.Signer);
 
-            // Azure DNS の TXT レコードを書き換え
             var zone = (await _dnsManagementClient.Zones.ListAllAsync()).First(x => challengeValidationDetails.DnsRecordName.EndsWith(x.Name));
 
             var resourceGroup = ExtractResourceGroup(zone.Id);
 
-            // Challenge の詳細から Azure DNS 向けにレコード名を作成
             var acmeDnsRecordName = challengeValidationDetails.DnsRecordName.Replace("." + zone.Name, "");
 
             RecordSet recordSet;
@@ -181,12 +170,10 @@ namespace KeyVault.Acmebot
 
                 recordSet.TTL = 60;
 
-                // 既存の TXT レコードに値を追加する
                 recordSet.TxtRecords.Add(new TxtRecord(new[] { challengeValidationDetails.DnsRecordValue }));
             }
             else
             {
-                // 新しく TXT レコードを作成する
                 recordSet = new RecordSet
                 {
                     TTL = 60,
@@ -214,20 +201,17 @@ namespace KeyVault.Acmebot
         [FunctionName(nameof(CheckDnsChallenge))]
         public async Task CheckDnsChallenge([ActivityTrigger] ChallengeResult challenge)
         {
-            // 実際に ACME の TXT レコードを引いて確認する
             var queryResult = await _lookupClient.QueryAsync(challenge.DnsRecordName, QueryType.TXT);
 
             var txtRecords = queryResult.Answers
                                         .OfType<DnsClient.Protocol.TxtRecord>()
                                         .ToArray();
 
-            // レコードが存在しなかった場合はエラー
             if (txtRecords.Length == 0)
             {
                 throw new RetriableActivityException($"{challenge.DnsRecordName} did not resolve.");
             }
 
-            // レコードに今回のチャレンジが含まれていない場合もエラー
             if (!txtRecords.Any(x => x.Text.Contains(challenge.DnsRecordValue)))
             {
                 throw new RetriableActivityException($"{challenge.DnsRecordName} value is not correct.");
@@ -243,13 +227,11 @@ namespace KeyVault.Acmebot
 
             if (orderDetails.Payload.Status == "pending")
             {
-                // pending の場合はリトライする
                 throw new RetriableActivityException("ACME domain validation is pending.");
             }
 
             if (orderDetails.Payload.Status == "invalid")
             {
-                // invalid の場合は最初から実行が必要なので失敗させる
                 throw new InvalidOperationException("Invalid order status. Required retry at first.");
             }
         }
@@ -259,7 +241,6 @@ namespace KeyVault.Acmebot
         {
             var acmeProtocolClient = await _acmeProtocolClientFactory.CreateClientAsync();
 
-            // Answer の準備が出来たことを通知
             foreach (var challenge in challenges)
             {
                 await acmeProtocolClient.AnswerChallengeAsync(challenge.Url);
@@ -277,7 +258,6 @@ namespace KeyVault.Acmebot
 
             try
             {
-                // Key Vault を使って CSR を作成
                 var request = await _keyVaultClient.CreateCertificateAsync(_options.VaultBaseUrl, certificateName, new CertificatePolicy
                 {
                     X509CertificateProperties = new X509CertificateProperties
@@ -298,7 +278,6 @@ namespace KeyVault.Acmebot
                 csr = Convert.FromBase64String(base64Csr);
             }
 
-            // Order の最終処理を実行し、証明書を作成
             var acmeProtocolClient = await _acmeProtocolClientFactory.CreateClientAsync();
 
             var finalize = await acmeProtocolClient.FinalizeOrderAsync(orderDetails.Payload.Finalize, csr);
@@ -307,7 +286,6 @@ namespace KeyVault.Acmebot
 
             var certificateData = await httpClient.GetByteArrayAsync(finalize.Payload.Certificate);
 
-            // X509Certificate2Collection を作成
             var x509Certificates = new X509Certificate2Collection();
 
             x509Certificates.ImportFromPem(certificateData);
